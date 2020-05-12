0e5c87b327f7c6a3bd8f33d63cfe6b0ef46aa21e
Govind Sharma <govindsharmajsk@gmail.com>
2020-05-13 02:26:49 +0530

    modified s_run to run on GPU

--- s_run.sh
+++ s_run.sh
@@ -14,8 +14,7 @@ NEGFACTOR=5
 TESTRATIO=0.2
 MODELNAME='catsetmat'
 DIM=16
-which python
-python src/train_test_sampler.py --help
-# /home/swyamsingh/repos/catsetmat/csm_env/bin/python src/train_test_sampler.py --data_name $DATANAME --num_splits $NUMSPLITS --start_split $STARTSPLIT --neg_factor $NEGFACTOR --test_ratio $TESTRATIO
-# python src/embedding_storer.py --data_name $DATANAME --num_splits $NUMSPLITS --start_split $STARTSPLIT --dim $DIM
-# python src/main.py --data_name $DATANAME --num_splits $NUMSPLITS --start_split $STARTSPLIT --model_name $MODELNAME --dim $DIM
+
+python src/train_test_sampler.py --data_name $DATANAME --num_splits $NUMSPLITS --start_split $STARTSPLIT --neg_factor $NEGFACTOR --test_ratio $TESTRATIO
+python src/embedding_storer.py --data_name $DATANAME --num_splits $NUMSPLITS --start_split $STARTSPLIT --dim $DIM
+python src/main.py --data_name $DATANAME --num_splits $NUMSPLITS --start_split $STARTSPLIT --model_name $MODELNAME --dim $DIM
