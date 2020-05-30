669a24e91d6cc54e7e8277d8b8a2a24aba2988c7
Govind Sharma <govindsharmajsk@gmail.com>
2020-05-31 04:35:22 +0530

    Updated s_run

--- s_run.sh
+++ s_run.sh
@@ -2,13 +2,12 @@
 
 #SBATCH --job-name=csm
 #SBATCH --ntasks=3
-#SBATCH --time=10:00:00
+#SBATCH --time=22:00:00
 #SBATCH --output=csm%j.out
-#SBATCH --gres=gpu:4
-#SBATCH --partition=cl2_all_8G
+#SBATCH --partition=cl1_all_64C
 
 DATANAME="sample_mag_acm"
-NUMSPLITS=4
+NUMSPLITS=5
 STARTSPLIT=0
 NEGFACTOR=5
 TESTRATIO=0.2
@@ -16,6 +15,12 @@ MODELNAME='catsetmat'
 DIM=16
 LR=0.001
 
-# python src/train_test_sampler.py --data_name $DATANAME --num_splits $NUMSPLITS --start_split $STARTSPLIT --neg_factor $NEGFACTOR --test_ratio $TESTRATIO
-# python src/embedding_storer.py --data_name $DATANAME --num_splits $NUMSPLITS --start_split $STARTSPLIT --dim $DIM
+python src/train_test_sampler.py --data_name $DATANAME --num_splits $NUMSPLITS --start_split $STARTSPLIT --neg_factor $NEGFACTOR --test_ratio $TESTRATIO
+python src/embedding_storer.py --data_name $DATANAME --num_splits $NUMSPLITS --start_split $STARTSPLIT --dim $DIM
+python src/main.py --data_name $DATANAME --num_splits $NUMSPLITS --start_split $STARTSPLIT --model_name $MODELNAME --dim $DIM --lr $LR
+
+MODELNAME='fspool'
+python src/main.py --data_name $DATANAME --num_splits $NUMSPLITS --start_split $STARTSPLIT --model_name $MODELNAME --dim $DIM --lr $LR
+
+#MODELNAME='n2v'
 python src/main.py --data_name $DATANAME --num_splits $NUMSPLITS --start_split $STARTSPLIT --model_name $MODELNAME --dim $DIM --lr $LR
